9f401dcfe01db29cb00028350bc355d1961ad1f8
ilsilentii <travis.vdm@outlook.com>
2017-09-11 23:11:10 -0600

    made a few changes

--- download_avatars.js
+++ download_avatars.js
@@ -8,9 +8,13 @@ var password = token
 var repoOwner = process.argv[2] //Takes in User input
 var repoName = process.argv[3]
 
+var k = 0;
+
 function checkargs(owner, name) { // Checks to see if there is a valid input. If there is, invoke the getRepoContributers function
   if (owner === undefined || name === undefined) {
     console.log("Please input a valid repo Owner and Name!") //if user input is undefined, this message displays
+  } else if (process.argv.length > 4) {
+    console.log("There are too many arguments!") // This message appears if there are more than 2 arguements
   } else {
     console.log('Welcome to the Github Avatar Downloader!');
     getRepoContributors(repoOwner, repoName, avatar);
@@ -28,7 +32,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-
   request.get(options)
     .on('error', function(err) { //this function gets invoked if there is an error
       throw err;
@@ -45,24 +48,41 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 function avatar(string) { //function that loops through the entire repo and grabs all the links to the avatars and names
+
+  if (string.message == "Not Found") {
+    console.log("This is not a valid repository")
+    return null;
+  }
+
+
   for (i in string)
     downloadImageByURL(string[i].avatar_url, 'avatars', string[i].login);
 }
 
+function percent(num) {
+  console.log(num);
+}
+
 function downloadImageByURL(url, filePath, username) { // This function downloaded the files to an avatars folder and names the files to the login plus extensions of the file types (eg. png or jpeg)
+
+
   var stream = request.get(url)
     .on('error', function(err) {
       throw err;
     })
     .on('response', function(response) {
       extension = response.headers["content-type"].split("/")
-      console.log('Downloading image...');
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath);
+      }
+      console.log("Downloading Images ...")
       stream.pipe(fs.createWriteStream(filePath + "/" + username + "." + extension[1]));
     })
     .on('end', function(response) {
-      console.log('Download complete.');
+      console.log("Download Complete")
     })
 
 }
+
 
 checkargs(repoOwner, repoName) // invokes the check arguments function
